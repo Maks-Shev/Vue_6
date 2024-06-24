954c97563689c4df3bf2e899238b9a509fa5de05
Maksim Shevchenko <maks.shev_87@mail.ru>
2024-06-24 15:06:59 +0300

    lesson

---
+++ lesson/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <PaymentsDisplay :items="paymentsList" :info="infoText"/>
+    <AddList @addNewPay="addNewPay"/>
+  </div>
+</template>
+
+<script>
+import AddList from './components/AddList.vue'
+import PaymentsDisplay from './components/PaymentsDisplay.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PaymentsDisplay,
+    AddList
+  },
+  data () {
+    return {
+      paymentsList: []
+    }
+  },
+  methods: {
+    fetchData () {
+      return [
+        {
+          date: '28.03.2023',
+          category: 'Food',
+          value: 169
+        },
+        {
+          date: '24.03.2023',
+          category: 'Transport',
+          value: 360
+        },
+        {
+          date: '24.03.2023',
+          category: 'Food',
+          value: 532
+        }
+      ]
+    },
+    addNewPay (data) {
+      this.paymentsList.push(data)
+    }
+  },
+  created () {
+    this.paymentsList = this.fetchData()
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lesson/src/components/PaymentsDisplay.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div v-for="item in items" :key="item.id">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaymentsDisplay',
+  props: {
+    items: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+
+  data () {
+    return {
+
+    }
+  },
+
+  methods: {
+    fetchData () {
+      return [
+        {
+          date: '28.03.2023',
+          category: 'Food',
+          value: 169
+        },
+        {
+          date: '24.03.2023',
+          category: 'Transport',
+          value: 360
+        },
+        {
+          date: '24.03.2023',
+          category: 'Food',
+          value: 532
+        }
+      ]
+    }
+  }
+}
+</script>
